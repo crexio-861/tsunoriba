ce418ac1e1a2a6f36b60e3dab69b8c61a67378c4
yamagata <crexio_861@crexio.co.jp>
2018-04-21 02:04:30 +0900

    初回コミット

---
+++ src/main/java/com/tsunoriba/TsunoribaApplication.java
@@ -0,0 +1,28 @@
+package com.tsunoriba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TsunoribaApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsunoribaApplication.class, args);
+	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TsunoribaApplication.class);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getSomething() {
+        return "ツノリバ！";
+    }
+}
